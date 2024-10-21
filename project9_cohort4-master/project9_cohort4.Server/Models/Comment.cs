d8af0b9b7ace674fb11208cb1fb6eb04b7ad15a8
Batool-Khazali <batoul.khazali@hotmail.com>
2024-10-21 16:25:03 +0300

    packages and database

---
+++ project9_cohort4-master/project9_cohort4.Server/Models/Comment.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace project9_cohort4.Server.Models;
+
+public partial class Comment
+{
+    public int CommentId { get; set; }
+
+    public int PostId { get; set; }
+
+    public int UserId { get; set; }
+
+    public string? Content { get; set; }
+
+    public DateTime? CreateDate { get; set; }
+
+    public virtual Post Post { get; set; } = null!;
+
+    public virtual ICollection<Reply> Replies { get; set; } = new List<Reply>();
+
+    public virtual User User { get; set; } = null!;
+}
