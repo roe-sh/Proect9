f76d71f8cbf9900a3a2f40a2d94c7bb36b2d5ac3
Batool-Khazali <batoul.khazali@hotmail.com>
2024-10-22 16:14:29 +0300

    profile controller

--- project9_cohort4-master/project9_cohort4.Server/Controllers/ProfileController.cs
+++ project9_cohort4-master/project9_cohort4.Server/Controllers/ProfileController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
 using project9_cohort4.Server.DTOs;
 using project9_cohort4.Server.Models;
+using project9_cohort4.Server.Services;
 
 namespace project9_cohort4.Server.Controllers
 {
@@ -43,19 +44,67 @@ namespace project9_cohort4.Server.Controllers
         }
 
 
-        //[HttpPut("editProfileInfo/{userId}")]
-        //public IActionResult editProfileInfo (int userId, [FromForm] editInfoDTO info)
-        //{
-        //    if (userId <= 0) return NotFound("inavlid id");
+        [HttpPut("editProfileInfo/{userId}")]
+        public async Task<IActionResult> editProfileInfo (int userId, [FromForm] editInfoDTO info)
+        {
+            if (userId <= 0) return NotFound("inavlid id");
+            if (info == null) return BadRequest("invalid input");
 
-        //    var user = _db.Users.FirstOrDefault(x => x.UserId == userId);
+            var user = _db.Users.FirstOrDefault(x => x.UserId == userId);
 
-        //    if (user == null) return NotFound("no user was found for this id");
+            if (user == null) return NotFound("no user was found for this id");
+
+            if (info.Image != null || info.Image.Length> 0)
+            {
+                var folder = Path.Combine(Directory.GetCurrentDirectory(), "images");
+
+                if (!Directory.Exists(folder)) Directory.CreateDirectory(folder);
+
+                var fileImage = Path.Combine(folder, info.Image.FileName);
+
+                using (var stream = new FileStream(fileImage, FileMode.Create))
+                {
+                    await info.Image.CopyToAsync(stream);
+                }
+
+                user.Image = info.Image.FileName ?? user.Image;
+                _db.Update(user);
+            }
+
+            user.FullName = info.Name ?? user.FullName;
+            user.Image = user.Image;
+            user.Address = info.Address ?? user.Address;
+            user.Phone = info.Phone ?? user.Phone;
+
+            _db.Update(user);
+            _db.SaveChanges();
+            return Ok("user info edited successfully");
+        }
 
 
+        [HttpPost("editPassword/{userId}")]
+        public IActionResult editPassword(int userId, [FromForm] editPasswordDTO input)
+        {
+            if (userId <= 0) return NotFound("inavlid id");
+            if (input == null) return BadRequest("invalid input");
 
-        //}
+            var user = _db.Users.FirstOrDefault(x => x.UserId == userId);
 
+            if (user == null) return NotFound("no user was found");
+
+            // Verify the old password
+            if (!PasswordHasher.VerifyPasswordHash(input.OldPassword, user.PasswordHash, user.PasswordSalt))
+                return Unauthorized("Old password is incorrect.");
+
+            // Hash the new password
+            PasswordHasher.CreatePasswordHash(input.NewPassword, out byte[] passwordHash, out byte[] passwordSalt);
+
+            user.PasswordHash = passwordHash;
+            user.PasswordSalt = passwordSalt;
+
+            _db.SaveChanges();
+            return Ok("password edited successfully");
+        }
 
 
 
---
+++ project9_cohort4-master/project9_cohort4.Server/DTOs/editPasswordDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace project9_cohort4.Server.DTOs
+{
+    public class editPasswordDTO
+    {
+        public string? OldPassword { get; set; }
+
+        public string? NewPassword { get; set; }
+
+    }
+}
