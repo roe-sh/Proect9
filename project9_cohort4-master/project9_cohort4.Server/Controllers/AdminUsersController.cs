12acacfc09a96aec500bfaacca560683ddf3b0f5
Batool-Khazali <batoul.khazali@hotmail.com>
2024-10-23 16:32:11 +0300

    finished api

---
+++ project9_cohort4-master/project9_cohort4.Server/Controllers/AdminUsersController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using project9_cohort4.Server.Models;
+
+namespace project9_cohort4.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdminUsersController : ControllerBase
+    {
+
+        private readonly MyDbContext _db;
+
+        public AdminUsersController(MyDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpPut("assignUserAsAdmin/{userId}")]
+        public IActionResult assignUserAsAdmin (int userId)
+        {
+            if (userId <= 0) return BadRequest("invalid id");
+
+            var user = _db.Users.FirstOrDefault(x => x.UserId == userId);
+
+            if (user == null) return NotFound("the user was not found");
+
+            user.IsAdmin = true;
+
+            _db.Users.Update(user);
+            _db.SaveChanges();
+
+            return Ok($"user with id {userId} was assined as admin");
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
