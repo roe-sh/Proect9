b3605be10cbe994db2c5cfa199bafb99fce3826d
Esraa Odat <esraaodat123123@gmail.com>
2024-10-25 11:55:55 +0300

    k

--- project9_cohort4-master/project9_cohort4.client/src/app/app.module.ts
+++ project9_cohort4-master/project9_cohort4.client/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AuthGuard } from './Adoptioh/auth.guard';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -71,6 +71,8 @@ import { SheltersComponent } from './Adoptioh/shelters/shelters.component';
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
+    ReactiveFormsModule,
+
     RouterModule.forRoot([
       { path: "", component: HomeComponent, pathMatch: "full" },
       { path: "getAllUsers", component: GetAllComponent },
