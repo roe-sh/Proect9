cb549bc89d287b05912b5cd250f54a1d9d893893
Esraa Odat <esraaodat123123@gmail.com>
2024-10-26 01:56:43 +0300

    es

--- project9_cohort4-master/project9_cohort4.client/src/app/Admin/add-shelters/add-shelters.component.ts
+++ project9_cohort4-master/project9_cohort4.client/src/app/Admin/add-shelters/add-shelters.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { UrlServiceService } from '../../batool/services/url-service.service';
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-add-shelters',
@@ -6,5 +9,37 @@ import { Component } from '@angular/core';
   styleUrl: './add-shelters.component.css'
 })
 export class AddSheltersComponent {
+  ngOnInit() { }
+  constructor(private _ser: UrlServiceService, private _router: Router) {
+  }
 
+
+  addShelter(data: any) {
+    const form = new FormData();
+    for (let key in data) {
+      form.append(key, data[key]);
+    }
+
+    this._ser.AddShelter(form).subscribe(
+      () => {
+        Swal.fire({
+          icon: 'success',
+          title: 'Shelter Added',
+          text: 'The shelter has been added successfully!',
+          confirmButtonText: 'OK'
+        }).then(() => {
+          this._router.navigate(['adminDashBoard/AllShelters']);
+        });
+      },
+      error => {
+        console.error("Error Adding Shelter", error);
+        Swal.fire({
+          icon: 'error',
+          title: 'Error',
+          text: 'There was an error adding the shelter. Please try again.',
+          confirmButtonText: 'OK'
+        });
+      }
+    );
+  }
 }
--- project9_cohort4-master/project9_cohort4.client/src/app/batool/services/url-service.service.ts
+++ project9_cohort4-master/project9_cohort4.client/src/app/batool/services/url-service.service.ts
@@ -17,6 +17,10 @@ export class UrlServiceService {
 
     return this.http.get<any>(`${this.baseUrl}/Animals1/${id}`)
   }
+  getShelterById(id :any) : Observable<any> {
+
+    return this.http.get<any>(`${this.baseUrl}/Shelters/${id}`)
+  }
 
   addAnimal(data :any): Observable<any> {
     return this.http.post<any>(`${this.baseUrl}/Animals1`, data)
@@ -44,8 +48,18 @@ export class UrlServiceService {
     return this.http.post<any>(`${this.baseUrl}/Categories/AddCategory`, data)
   }
 
+  AddShelter(data: any): Observable<any> {
+    return this.http.post<any>(`${this.baseUrl}/Shelters`, data)
+  }
+
   UpdateCategory(id: any, data: any): Observable<any> {
     return this.http.put(`${this.baseUrl}/Categories/UpdateCategory/${id}`, data);
+  }
+
+
+  
+  UpdateShelter(id: any, data: any): Observable<any> {
+    return this.http.put(`${this.baseUrl}/Shelters/${id}`, data);
   }
 
 
