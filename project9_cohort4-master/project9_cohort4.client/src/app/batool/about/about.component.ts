bc1e154e76ee1d9343c1f6ba31c6d817eaa43335
Batool-Khazali <batoul.khazali@hotmail.com>
2024-10-26 18:31:19 +0300

    nav and about us page done

--- project9_cohort4-master/project9_cohort4.client/src/app/batool/about/about.component.ts
+++ project9_cohort4-master/project9_cohort4.client/src/app/batool/about/about.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import Swal from 'sweetalert2';
+import { BUrlServicesService } from '../../Batoul/burl-services.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-about',
@@ -11,19 +14,90 @@ export class AboutComponent {
       name: 'Esraa Odat',
       position: 'Scrum Master',
       image: 'team-1.png', 
-      description: 'An experienced Developer who loves helping others.'
+      description: 'A dedicated Scrum Master with a passion for agile methodologies and team collaboration.'
     },
     {
       name: 'Batool Khazali',
       position: 'Product Owner',
       image: 'team-2.png',
-      description: 'A passionate Developer who ensures Website look it best.'
+      description: 'An experienced Product Owner who excels at defining product vision and prioritizing tasks.'
     },
     {
       name: 'Rahaf AL-Shorman',
       position: 'Web Developer',
       image: 'team-3.png',
-      description: 'Specializes in pet website developing.'
+      description: 'A skilled Web Developer with expertise in creating responsive and user-friendly applications.'
+    },
+    {
+      name: 'Mostafa Al-Momani',
+      position: 'Web Developer',
+      image: 'team-3.png',
+      description: 'A passionate Web Developer who specializes in front-end technologies and enjoys solving complex problems.'
+    },
+    {
+      name: 'Malik Ibdah',
+      position: 'Web Developer',
+      image: 'team-3.png',
+      description: 'A creative Web Developer focused on building innovative solutions and enhancing user experiences.'
     }
   ];
+
+
+  ngOnInit() {
+
+  }
+
+  constructor(private _ser: BUrlServicesService) { }
+
+  sendEmail(data: any) {
+    var form = new FormData();
+
+    for (let key in data) {
+      form.append(key, data[key])
+    }
+
+    this._ser.sendMessage(form).subscribe(
+      () => {
+        Swal.fire({
+          title: "Your message was sent successfully",
+          text: "We'll replay to you soon",
+          icon: "success",
+          showClass: {
+            popup: `
+          animate__animated
+          animate__fadeInUp
+          animate__faster
+        `
+          },
+          hideClass: {
+            popup: `
+          animate__animated
+          animate__fadeOutDown
+          animate__faster
+        `
+          }
+        });
+      },
+      error => {
+        Swal.fire({
+          title: "Error!",
+          text: "There was a problem sending your message.",
+          icon: "error",
+        });
+      }
+    );
+
+  }
+
+
+  onSubmit(form: NgForm) {
+    if (form.invalid) {
+      console.log('Form is invalid:', form);
+      return;
+    }
+    console.log('Form is valid:', form);
+  }
+
+
+
 }
